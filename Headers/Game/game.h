307442304a75b8b01c8271f863b69d51a7d053d2
samuelnub <notsamnub@gmail.com>
2016-07-31 00:50:08 +0100

    settings

--- Headers/Game/game.h
+++ Headers/Game/game.h
@@ -2,6 +2,7 @@
 #ifndef GAME_GAME_H
 #define GAME_GAME_H
 
+#include <Handler/settings.h>
 #include <Handler/input.h>
 #include <Handler/window.h>
 
@@ -15,6 +16,7 @@ public:
 
 
 	// Public classes for everyone to get stuff from
+	Settings settings;
 	Input input;
 	Window window;
 
