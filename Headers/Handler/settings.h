20ed5b54f1ae86d621858e2dd0f6a21e9d553948
samuelnub <notsamnub@gmail.com>
2016-07-31 00:50:42 +0100

    settings class stores settings as map of strings, and has getters which lexically cast

---
+++ Headers/Handler/settings.h
@@ -0,0 +1,62 @@
+#pragma once
+#ifndef HANDLER_SETTINGS_H
+#define HANDLER_SETTINGS_H
+
+#include <tinyxml2/tinyxml2.h>
+
+#include <unordered_map>
+#include <string>
+#include <boost/lexical_cast.hpp>
+#include <memory>
+
+class Game;
+
+class Settings
+{
+public:
+	Settings();
+	~Settings();
+
+	void init(Game *game);
+
+	int write(std::string settingName, std::string value);
+
+	// Get your setting, casted to your defined type
+	inline bool getBool(std::string name)
+	{
+		return boost::lexical_cast<bool>(this->_settings.at(name));
+	}
+
+	inline int getInt(std::string name)
+	{
+		return boost::lexical_cast<int>(this->_settings.at(name));
+	}
+
+	inline float getFloat(std::string name)
+	{
+		return boost::lexical_cast<float>(this->_settings.at(name));
+	}
+
+	inline std::string getString(std::string name)
+	{
+		return this->_settings.at(name);
+	}
+
+private:
+	Game *_game;
+
+	const char *_filePath;
+
+	std::unique_ptr<tinyxml2::XMLDocument> _xmlDoc;
+
+	std::unordered_map<std::string, std::string> _settings;
+
+	// initial read to map
+	int map();
+
+protected:
+
+
+};
+
+#endif
