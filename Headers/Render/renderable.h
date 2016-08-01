c8ae7c6717c31dc539455114fef1cf0ad25e132a
samuelnub <notsamnub@gmail.com>
2016-08-01 15:57:20 +0100

    renderable class

---
+++ Headers/Render/renderable.h
@@ -0,0 +1,161 @@
+#pragma once
+#ifndef RENDER_SPRITE_H
+#define RENDER_SPRITE_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <string>
+#include <unordered_map>
+
+class Game;
+
+template <typename T>
+struct Vec2
+{
+	T x;
+	T y;
+
+	Vec2()
+	{
+		this->x = 0;
+		this->y = 0;
+	}
+
+	Vec2(T x, T y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+};
+
+// Animation struct for textures, if x and y is 1, its a still frame, aka a non-animated sprite, animations go left to right, then top to bottom, x gets looped, then y increments, etc...
+struct Animation
+{
+	Vec2<int> xyFrameCount;
+	Vec2<int> initialOffset; // animation sheets would most likely be stacked downward, toward +y, so x should most likely be 0 in all cases.
+	
+	int updateTime; // in ms
+
+	Animation() // default constructor assumes its not animated
+	{
+		this->xyFrameCount = Vec2<int>();
+		this->initialOffset = Vec2<int>();
+		this->updateTime = 60;
+	}
+
+	Animation(Vec2<int> xyCount, Vec2<int> offset, int time)
+	{
+		this->xyFrameCount = xyCount;
+		this->initialOffset = offset;
+		this->updateTime = time;
+	}
+};
+
+// Sprite/tile/whatever textured component for your actor
+class Renderable
+{
+public:
+	Renderable();
+	~Renderable();
+	
+	void init(Game *game, std::string filePath, Vec2<int> sourceXY, Vec2<int> sourceWH, Vec2<int> destXY, Vec2<int> destWH, Vec2<float> scale = Vec2<float>(1.0f,1.0f), float angle = 0, Vec2<int> origin = Vec2<int>(), bool visible = true);
+
+	void init(Game *game, std::string filePath, SDL_Rect sourceRect, SDL_Rect destRect, Vec2<float> scale = Vec2<float>(1.0f, 1.0f), float angle = 0, SDL_Point origin = SDL_Point(), bool visible = true);
+
+	inline SDL_Texture **getTexture()
+	{
+		return &this->_texture;
+	}
+	
+	// Mostly redundant, for manual setting
+	inline void setSourceRect(SDL_Rect rect)
+	{
+		this->_sourceRect = rect;
+	}
+
+	inline SDL_Rect getSourceRect()
+	{
+		return this->_sourceRect;
+	}
+
+	// Mostly redundant, for manual setting. You'd most likely want to use those transform functions below
+	inline void setDestRect(SDL_Rect rect)
+	{
+		this->_destRect = rect;
+	}
+
+	inline SDL_Rect getDestRect()
+	{
+		return this->_destRect;
+	}
+
+	// utilised elapsed time from input to animate
+	void tick();
+
+	// additive translation
+	void translate(int x, int y);
+	void translate(Vec2<int> xy);
+
+	// scale around origin
+	void scale(float xFactor, float yFactor, bool keepCentred = true);
+	void scale(Vec2<float> factors, bool keepCentred = true);
+
+	// additive rotation
+	void rotate(float angle, Vec2<float> origin);
+	void rotate(float angle, SDL_Point origin);
+	void rotate(float angle);
+
+	void flip(SDL_RendererFlip flip);
+
+	void storeAnim(std::string name, Animation anim);
+	void useAnim(std::string name, bool repeat = true);
+
+	inline void setAnimating(bool animate)
+	{
+		this->_isAnimating = animate;
+	}
+
+	inline bool isAnimating()
+	{
+		return this->_isAnimating;
+	}
+
+	inline void setVisible(bool vis)
+	{
+		this->_visible = vis;
+	}
+
+	inline bool isVisible()
+	{
+		return this->_visible;
+	}
+
+protected:
+
+
+private:
+	Game *_game;
+
+	SDL_Texture *_texture;
+	SDL_Rect _sourceRect;
+	SDL_Rect _destRect;
+
+	Vec2<float> _scale;
+	double _angle;
+	SDL_Point _origin; // i prefer steam
+	SDL_RendererFlip _flipFlag; // its an enum
+	
+	bool _isAnimated; // false on init, assumes its a static renderable
+	std::unordered_map<std::string, Animation> _animations;
+	std::string _currentAnim; // current animation this instance is set to, each animation is stacked downward, toward +y for SDL
+	bool _curAnimRepeat;
+	Vec2<int> _curFrameIndex;
+	bool _isAnimating;
+	int _timeElapsed; // accumulates until the animation's specified time is consumed, then go to next frame
+
+	bool _visible;
+
+	void animate();
+};
+
+#endif
---
+++ Source/Render/renderable.cpp
@@ -0,0 +1,202 @@
+#include <Render/renderable.h>
+#include <Game/game.h>
+#include <iostream>
+#include <boost/math/special_functions/round.hpp>
+
+Renderable::Renderable()
+{
+}
+
+Renderable::~Renderable()
+{
+}
+
+void Renderable::init(Game * game, std::string filePath, Vec2<int> sourceXY, Vec2<int> sourceWH, Vec2<int> destXY, Vec2<int> destWH, Vec2<float> scale, float angle, Vec2<int> origin, bool visible)
+{
+	this->_game = game;
+
+	this->_texture = IMG_LoadTexture(*this->_game->window.getRenderer(), filePath.c_str());
+	if (this->_texture == nullptr)
+	{
+		std::cout << "Couldn't load texture with the file path of " << filePath << "!\n";
+	}
+
+	this->_sourceRect.x = destXY.x;
+	this->_sourceRect.y = destXY.y;
+	this->_sourceRect.w = destWH.x;
+	this->_sourceRect.h = destWH.y;
+
+	this->_destRect.x = destXY.x;
+	this->_destRect.y = destXY.y;
+	this->_destRect.w = destWH.x;
+	this->_destRect.h = destWH.y;
+
+	this->_scale = scale;
+	this->_angle = angle;
+	
+	this->_origin.x = origin.x;
+	this->_origin.y = origin.y;
+	this->scale(this->_scale);
+	this->rotate(this->_angle);
+
+	this->_flipFlag = SDL_FLIP_NONE;
+
+	this->_isAnimated = false;
+	this->_curAnimRepeat = true;
+	this->_curFrameIndex = Vec2<int>();
+}
+
+void Renderable::init(Game * game, std::string filePath, SDL_Rect sourceRect, SDL_Rect destRect, Vec2<float> scale, float angle, SDL_Point origin, bool visible)
+{
+	this->_game = game;
+
+	this->_texture = IMG_LoadTexture(*this->_game->window.getRenderer(), filePath.c_str());
+	if (this->_texture == nullptr)
+	{
+		std::cout << "Couldn't load texture with the file path of " << filePath << "!\n";
+	}
+
+	this->_sourceRect = sourceRect;
+	this->_destRect = destRect;
+
+	this->_scale = scale;
+	this->_angle = angle;
+
+	this->_origin = origin;
+	this->scale(this->_scale);
+	this->rotate(this->_angle);
+
+	this->_flipFlag = SDL_FLIP_NONE;
+
+	this->_isAnimated = false;
+	this->_curAnimRepeat = true;
+	this->_curFrameIndex = Vec2<int>();
+}
+
+void Renderable::tick()
+{
+	if (this->_isAnimating)
+	{
+		this->animate();
+	}
+	if (this->_visible)
+	{
+		SDL_RenderCopyEx(*this->_game->window.getRenderer(), this->_texture, &this->_sourceRect, &this->_destRect, this->_angle, &this->_origin, this->_flipFlag);
+	}
+}
+
+void Renderable::translate(int x, int y)
+{
+	this->_destRect.x += x;
+	this->_destRect.y += y;
+}
+
+void Renderable::translate(Vec2<int> xy)
+{
+	this->_destRect.x += xy.x;
+	this->_destRect.y += xy.y;
+}
+
+void Renderable::scale(float xFactor, float yFactor, bool keepCentred)
+{
+	this->_scale.x = xFactor;
+	this->_scale.y = yFactor;
+
+	if (keepCentred)
+	{
+		this->translate(Vec2<int>(
+			boost::math::iround<>(this->_destRect.w / xFactor),
+			boost::math::iround<>(this->_destRect.h / yFactor)
+			));
+	}
+
+	this->_destRect.w *= this->_scale.x;
+	this->_destRect.h *= this->_scale.y;
+}
+
+void Renderable::scale(Vec2<float> factors, bool keepCentred)
+{
+	this->_scale = factors;
+
+	if (keepCentred)
+	{
+		this->translate(Vec2<int>(
+			boost::math::iround<>(this->_destRect.w / factors.x),
+			boost::math::iround<>(this->_destRect.h / factors.y)
+			));
+	}
+
+	this->_destRect.w *= this->_scale.x;
+	this->_destRect.h *= this->_scale.y;
+}
+
+void Renderable::rotate(float angle, Vec2<float> origin)
+{
+	this->_angle += angle;
+
+	this->_origin.x = origin.x;
+	this->_origin.y = origin.y;
+}
+
+void Renderable::rotate(float angle, SDL_Point origin)
+{
+	this->_angle += angle;
+	
+	this->_origin = origin;
+}
+
+void Renderable::rotate(float angle)
+{
+	this->_angle += angle;
+}
+
+void Renderable::flip(SDL_RendererFlip flip)
+{
+	this->_flipFlag = flip;
+}
+
+void Renderable::animate()
+{
+	this->_timeElapsed += this->_game->input.getDelta();
+
+	if (this->_timeElapsed > this->_animations.at(this->_currentAnim).updateTime)
+	{
+		this->_timeElapsed -= this->_animations.at(this->_currentAnim).updateTime;
+		if (this->_curFrameIndex.x < this->_animations.at(this->_currentAnim).xyFrameCount.x)
+		{
+			this->_curFrameIndex.x++;
+		}
+		else if (this->_curFrameIndex.x >= this->_animations.at(this->_currentAnim).xyFrameCount.x ||
+			this->_curFrameIndex.y < this->_animations.at(this->_currentAnim).xyFrameCount.y)
+		{
+			this->_curFrameIndex.x = 0;
+			this->_curFrameIndex.y++;
+		}
+		else if(this->_curAnimRepeat == true)
+		{
+			this->_curFrameIndex = Vec2<int>();
+		}
+		else
+		{
+			;
+		}
+	}
+
+	this->_sourceRect.x = this->_animations.at(this->_currentAnim).initialOffset.x +
+		(this->_curFrameIndex.x * this->_sourceRect.w);
+	this->_sourceRect.y = this->_animations.at(this->_currentAnim).initialOffset.y +
+		(this->_curFrameIndex.y * this->_sourceRect.h);
+}
+
+void Renderable::storeAnim(std::string name, Animation anim)
+{
+	this->_isAnimated = true;
+	this->_animations[name] = anim;
+}
+
+void Renderable::useAnim(std::string name, bool repeat)
+{
+	this->_currentAnim = name;
+
+	this->_curAnimRepeat = repeat;
+}
